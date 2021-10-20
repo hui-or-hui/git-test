852fe2843ecda9b32219ee4f9d701235cfdf7b51
Layne <Layne@huihui.com>
2021-10-20 20:28:59 +0800

    second commit

--- src/main/java/com/huihui/test/Gittest.java
+++ src/main/java/com/huihui/test/Gittest.java
@@ -7,5 +7,6 @@ package com.huihui.test;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello");
     }
 }
