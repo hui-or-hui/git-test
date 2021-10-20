ddb0fe7197adfd9c1adc86d6bd0e7b6bc1a1275a
Layne <Layne@huihui.com>
2021-10-20 20:26:53 +0800

    first commit

---
+++ src/main/java/com/huihui/test/Gittest.java
@@ -0,0 +1,11 @@
+package com.huihui.test;
+
+/**
+ * @author huihui
+ * @create 2021-10-20-20:24
+ */
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
